5cd59fb8cfc6e311ccf8c250c06137d93df85df7
vgulaev <vgulaev@yandex.ru>
2013-02-15 11:13:28 +0600

    Create Lorem Ipsum

---
+++ html5pure.py
@@ -0,0 +1,7 @@
+class html5pure:
+    def firstmethod(self):
+        print("I do something")
+        
+#print("first code line")
+#test = html5pure()
+#test.firstmethod()
---
+++ testhtml5.py
@@ -0,0 +1,7 @@
+from html5pure import html5pure 
+
+p = html5pure()
+
+p.firstmethod()
+        
+#print("first code line")
